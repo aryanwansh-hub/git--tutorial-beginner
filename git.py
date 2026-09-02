e1392ef808022cd8b33218875b3ef910cb48e9ac
Aryan <aryanwansh@gmail.com>
2026-09-02 23:58:26 +0530

    initial commit

---
+++ git.py
@@ -0,0 +1,4 @@
+first download git file from google on git-scm.com
+then next next next without changing anything in the installation window then uncheck both at last and finish the installation.
+right click in folder or repository which you use git and click open gitbash here
+
