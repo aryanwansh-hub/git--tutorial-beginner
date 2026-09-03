d409945d034852f5603de3dc40c7df0137b309d2
Aryan <aryanwansh@gmail.com>
2026-09-03 23:12:09 +0530

    contact.html

--- git.py
+++
@@ -1,4 +0,0 @@
-first download git file from google on git-scm.com
-then next next next without changing anything in the installation window then uncheck both at last and finish the installation.
-right click in folder or repository which you use git and click open gitbash here
-
